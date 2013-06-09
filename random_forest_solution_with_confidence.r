c0d502dfb6d1e38688d6781be456634b7c3c02d8
Eric Chio <eric@bhotels.me>
2013-06-09 19:18:06 +0800

    Initial response with rank 191 and rank 680.

---
+++ random_forest_solution_with_confidence.r
@@ -0,0 +1,19 @@
+﻿# This uses random forest to recognize biological response
+# Test on real data.
+# This yields an average rank of #191 with a log loss of 0.39895
+
+setwd('D:/L/source/biological response') # only for my computer
+
+library(randomForest)
+
+train_data <- read.csv('data/train.csv', header = T)
+test_data <- read.csv('data/test.csv', header = T)
+
+train_set <- train_data
+train_features <- train_set[, -1]
+train_labels <- train_set[, 1]
+
+rf <- randomForest(train_features, train_labels)
+test.predict <- predict(rf, test_data)
+
+write.table(sprintf("%.6f", test.predict), 'random_forest_solution_with_confidence.txt', sep=',', row.names=FALSE, col.names=FALSE, quote=FALSE)
---
+++ random_forest_training.r
@@ -0,0 +1,25 @@
+﻿# This uses random forest to recognize biological response
+# Use only on train data to see results
+
+setwd('D:/L/source/biological response') # only for my computer
+
+library(randomForest)
+
+data <- read.csv('data/train.csv', header = T)
+
+n <- dim(data)[1]
+
+train_indices <- c(1:as.integer(n * 3 / 4))
+train_set <- data[train_indices,]
+train_features <- train_set[, -1]
+train_labels <- train_set[, 1]
+test_set <- data[-train_indices,]
+test_features <- test_set[, -1]
+test_labels <- test_set[, 1]
+
+rf <- randomForest(train_features, as.factor(train_labels))
+test.predict <- predict(rf, test_features)
+accuracy <- sum(test.predict == test_labels)/length(test_labels)
+
+cat("Accuracy = ", accuracy, "\n")
+
