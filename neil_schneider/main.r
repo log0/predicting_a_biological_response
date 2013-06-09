8ca44e0cb88e32cbfbbce9207a439d48ec0bc953
Eric Chio <eric@bhotels.me>
2013-06-09 22:05:56 +0800

    Add more other competitors' code for studying.

---
+++ neil_schneider/main.r
@@ -0,0 +1,28 @@
+﻿superman <- makeCluster(7)
+registerDoSNOW(superman)
+getDoParRegistered(); getDoParName(); getDoParWorkers();
+
+###Run 28 oRF.pls models
+oRF.pls.cvs <- foreach(
+  i=1:nfolds
+  ,.packages='obliqueRF'
+  ,.verbose=TRUE
+  ) %dopar% {
+    #i<-1L
+    train.flag <- (fold.ids != i)
+    test.flag <- (fold.ids == i)
+    
+    ###Pass the gbm the out of fold data too to save time
+    trash.oRF <- obliqueRF(
+      x=as.matrix(train[train.flag,])
+      ,y=as.numeric(outcome[train.flag])
+      ,mtry=250
+      ,ntree=500
+      ,training_method="pls"
+      )
+    oRF.fold.pred <- predict(trash.oRF,train[test.flag,],type="prob")
+    return(oRF.fold.pred[,2])
+  }
+
+stopCluster(superman)
+stop.time <- date()
